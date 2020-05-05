bf9f623565b640806f487b92c8cab10d186776b2
Omarb28 <omarb28@gmail.com>
2020-05-05 12:42:28 +0000

    Updated the view for Artist editing

--- app.py
+++ app.py
@@ -529,22 +529,16 @@ def show_artist(artist_id):
 #  ----------------------------------------------------------------
 @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
+  # DONE: populate form with fields from artist with ID <artist_id>
   form = ArtistForm()
-  artist={
-    "id": 4,
-    "name": "Guns N Petals",
-    "genres": ["Rock n Roll"],
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "326-123-5000",
-    "website": "https://www.gunsnpetalsband.com",
-    "facebook_link": "https://www.facebook.com/GunsNPetals",
-    "seeking_venue": True,
-    "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-    "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
-  }
-  # TODO: populate form with fields from artist with ID <artist_id>
-  return render_template('forms/edit_artist.html', form=form, artist=artist)
+
+  artist = Artist.query.get(artist_id)
+  if artist is None:
+    abort(404)
+
+  genres = [g.genre for g in artist.genres]
+
+  return render_template('forms/edit_artist.html', form=form, artist=artist, genres=genres)
 
 @app.route('/artists/<int:artist_id>/edit', methods=['POST'])
 def edit_artist_submission(artist_id):
@@ -554,28 +548,16 @@ def edit_artist_submission(artist_id):
   return redirect(url_for('show_artist', artist_id=artist_id))
 
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
-def edit_venue(venue_id):  
-  '''
-  venue_data={
-    "id": 1,
-    "name": "The Musical Hop",
-    "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-    "address": "1015 Folsom Street",
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "123-123-1234",
-    "website": "https://www.themusicalhop.com",
-    "facebook_link": "https://www.facebook.com/TheMusicalHop",
-    "seeking_talent": True,
-    "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-  }
-  '''
-  # TODO: populate form with values from venue with ID <venue_id>
+def edit_venue(venue_id):
+  # DONE: populate form with values from venue with ID <venue_id>
   form = VenueForm()
+
   venue = Venue.query.get(venue_id)
+  if venue is None:
+    abort(404)
+
   genres = [g.genre for g in venue.genres]
-  
+
   return render_template('forms/edit_venue.html', form=form, venue=venue, genres=genres)
 
 @app.route('/venues/<int:venue_id>/edit', methods=['POST'])
