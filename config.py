a1765ce9dab2b18a30dc3a52d3d7f367e94e1ece
Omarb28 <omarb28@gmail.com>
2020-05-09 13:23:24 +0000

    Changed database to be remotely hosted on Heroku

--- config.py
+++ config.py
@@ -7,5 +7,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
-SQLALCHEMY_DATABASE_URI = 'postgresql://vagrant:vagrant@localhost:5432/fyyur'
+
+# local database
+# SQLALCHEMY_DATABASE_URI = 'postgresql://vagrant:vagrant@localhost:5432/fyyur'
+
+# remote database
+SQLALCHEMY_DATABASE_URI = 'postgres://bsogzflyygtivv:f072c96d8750ab2813eca9ef2ca3cd7438609b886cdb592bc3a2221cec29cb39@ec2-23-22-156-110.compute-1.amazonaws.com:5432/d4ibbmtunji83t'
+
+# my heroku web app link:  https://fyyur-app-omar.herokuapp.com/
+# my github profile: 
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
